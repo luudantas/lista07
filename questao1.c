9e35bd4c7a13f2cf1d7484eeaef5b337b488e041
luudantas <luana.vsdantas@gmail.com>
2026-05-14 13:46:42 -0300

    Initial commit

---
+++ questao1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void recebeNota (float nota[10])
+{
+    
+    for(int i=0; i<10; i++)
+    {
+        scanf("%f",&nota[i]);
+    }
+    calculaMedia(nota);
+}
+void calculaMedia(float nota[10])
+{
+    int soma=0;
+    int cont=0, contmedia=0;
+    for(int j=0; j<10; j++)
+    {
+        soma+=nota[j];
+        cont++;
+        if(nota[j]>=6) contmedia++;
+    }
+    printf("Media: %.2f\n",soma/(1.0*cont));
+    printf("Alunos acima da media: %d\n",contmedia);
+}
+int main()
+{
+    float nota[10];
+    recebeNota(nota);
+    return 0;
+}
