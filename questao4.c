6965d142959c9e8f7a4333b03ac29dca46de3ca1
Luana Dantas <luana.vsdantas@gmail.com>
2026-05-15 12:15:27 -0300

    Update questao4.c

--- questao4.c
+++ questao4.c
@@ -3,5 +3,15 @@
 #include <string.h>
 int main()
 {
+    int N, maior;
+    scanf("%d",&N);
+    int *vet = (int *)malloc(N*sizeof(int));
+    for(int i=0,i<N;i++)
+    {
+        scanf("%d",vet[i]);
+        if(i==0) maior = vet[0];
+        else
+    }
+    free(vet);
     return 0;
 }
